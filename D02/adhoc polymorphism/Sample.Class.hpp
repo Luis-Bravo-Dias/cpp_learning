aed96ff4ac82a85b90b287af67f3226e5158f248
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-05-03 16:49:13 +0100

    8

--- D02/adhoc polymorphism/Sample.Class.hpp
+++ D02/adhoc polymorphism/Sample.Class.hpp
@@ -1,18 +1,18 @@
 #ifndef SAMPLE_CLASS_HPP
-# define SAMPLE_CLASS_HPP
+#define SAMPLE_CLASS_HPP
 
-# include <iostream>
+#include <iostream>
 
 class Sample
 {
-	public:
-		Sample(void);
-		~Sample(void);
+public:
+	Sample(void);
+	~Sample(void);
 
-	void	bar(char const c) const;
-	void	bar(int const n) const;
-	void	bar(float const z) const;
-	void	bar(Sample const & i) const;
+	void bar(char const c) const;
+	void bar(int const n) const;
+	void bar(float const z) const;
+	void bar(Sample const &i) const;
 };
 
 #endif
